b3a389e5d6f3a5c947ea30f287dd96134515b04c
Justin Balthrop <git@justinbalthrop.com>
2011-08-01 18:29:14 -0700

    add partition_by

---
+++ lib/rupture/seq.rb
@@ -0,0 +1,17 @@
+module Rupture::Seq
+  def split_at(n)
+    [take(n), drop(n)]
+  end
+
+  def partition_by
+    results  = []
+    previous = nil
+    each do |i|
+      current = yield(i)
+      results << [] if current != previous or results.empty?
+      results.last << i
+      previous = current
+    end
+    results
+  end
+end
