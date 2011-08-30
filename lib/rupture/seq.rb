4b0f5b779c651936bad4683465b9fea4fc2d2162
Justin Balthrop <git@justinbalthrop.com>
2011-08-29 18:32:33 -0700

    add partition, partition_all and partition_between

--- lib/rupture/seq.rb
+++ lib/rupture/seq.rb
@@ -3,9 +3,29 @@ module Enumerable
     [take(n), drop(n)]
   end
 
+  def partition(n, step = n, pad = nil)
+    results = []
+    coll = self
+
+    while coll.size >= n
+      results << coll.take(n)
+      coll = coll.drop(step)
+    end
+
+    if pad and coll.any?
+      results << coll + pad.take(n - coll.size)
+    end
+    results
+  end
+
+  def partition_all(n, step = n)
+    partition(n, step, [])
+  end
+
   def partition_by
     results  = []
     previous = nil
+
     each do |i|
       current = yield(i)
       results << [] if current != previous or results.empty?
@@ -15,11 +35,13 @@ module Enumerable
     results
   end
 
-  def partition_on(&block)
-    results  = []
-    each do |i|
-      results << [] if yield(i) or results.empty?
-      results.last << i
+  def partition_between
+    return [] if empty?
+    results = [[first]]
+
+    partition(2, 1).each do |(a,b)|
+      results << [] if yield(a,b)
+      results.last << b
     end
     results
   end
--- test/seq_test.rb
+++ test/seq_test.rb
@@ -1,15 +1,27 @@
 require File.dirname(__FILE__) + '/test_helper'
 
 class SeqTest < Test::Unit::TestCase
-  should "split_at index" do
+  should "split_at" do
     assert_equal [[1,2,3],[4,5,6]], [1,2,3,4,5,6].split_at(3)
   end
 
-  should "partition_by predicate" do
-    assert_equal [[1, 3], [2, 4], [3], [4, 8]], [1,3,2,4,3,4,8].partition_by {|i| i.odd?}
+  should "partition" do
+    assert_equal [[1,2],[3,4]],       [1,2,3,4,5].partition(2)
+    assert_equal [[1,2],[2,3],[3,4]], [1,2,3,4].partition(2,1)
+    assert_equal [[1,2],[4,5]],       [1,2,3,4,5].partition(2,3)
+    assert_equal [[1,2],[3,4],[5,6]], [1,2,3,4,5].partition(2,2,[6,6,6])
   end
 
-  should "partition_on predicate" do
-    assert_equal [[1], [nil, 4, 3], [nil, 8]], [1,nil,4,3,nil,8].partition_on {|i| i == nil}
+  should "partition_all" do
+    assert_equal [[1,2],[3,4],[5]], [1,2,3,4,5].partition_all(2)
+    assert_equal [[1,2],[2,3],[3]], [1,2,3].partition_all(2,1)
+  end
+
+  should "partition_by" do
+    assert_equal [[1,3], [2,4], [3], [4,8]], [1,3,2,4,3,4,8].partition_by {|i| i.odd?}
+  end
+
+  should "partition_between" do
+    assert_equal [[1], [nil, 4, 3], [nil, 8]], [1,nil,4,3,nil,8].partition_between {|a,b| b == nil}
   end
 end
